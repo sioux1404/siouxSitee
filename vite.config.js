000b6f221e3aabeb0a4cfb36e960651e809c9848
sioux <sioux1404@gmail.com>
2025-05-10 15:09:01 -0700

    Your commit message

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/siouxSitee/',  // <- set this to your GitHub repo name
   plugins: [react()],
 })
