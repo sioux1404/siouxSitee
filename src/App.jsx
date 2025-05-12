d2ced4c8dddd5b8a6f3f32d359faac6bca82eb51
sioux <sioux1404@gmail.com>
2025-05-12 13:46:38 -0700

    Your commit message here

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
 import './App.css';
 import Background from './assets/Background.png'; // Import the image
+import Header from './Headers'
 
 function App() {
   return (
     <div className="app-container">
+      <Header />
       <img src={Background} alt="Background" />
     </div>
   );
---
+++ src/Headers.jsx
@@ -0,0 +1,13 @@
+import './Header.css'
+function Header() {
+  return (
+    <header className="header">
+      <nav className="nav-bar">
+        <a href="#item1" className="nav-item">Hot Menu </a>
+        <a href="#item1" className="nav-item">Cold Menu </a>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
